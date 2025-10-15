192c0002fd5cba8ef1e1fa3e68fa0e76041ff747
Akash Kothari <akash@kotharigroup.com>
2025-10-15 14:33:48 -0400

    "HistoryManagement"

--- src/main.c
+++ src/main.c
@@ -2,8 +2,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h> 
+#include <strings.h> 
 #include <sys/wait.h>
+#include <termios.h>
 
+//colors for text
 #define RED     "\033[1;31m"
 #define GREEN   "\033[1;32m"
 #define YELLOW   "\033[1;33m"
@@ -13,21 +16,49 @@
 #define WHITE    "\033[1;37m"
 #define RESET   "\033[0m"
 
+//history
+char *history[1000];
+int history_index = 0;
+int history_cur_index = 0;
+void manageHistory(char *line) {
+    if (line[0] != '\0') {
+        history[history_index] = strdup(line);
+        history_index+=1;
+        history_cur_index = history_index;
+    }
+}
+
+//allow terminal to get real time handling for arrow key presses or tabs (so their not just chars)
+void enable_raw_mode() {
+    struct termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag &= ~(ICANON | ECHO);  // disable canonical mode & echo
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
+//states
+int hiState = 0;
 
 //SHELL BUILT IN COMMANDS LIST
 int shell_cd(char **args);
 int shell_help(char **args);
 int shell_exit(char **args);
+int shell_greet(char **args);
 //list of built in commands strings (to match)
 char *builtin_str[] = {
   "cd",
   "help",
-  "exit"
+  "exit",
+  "hi",
+  "hello",
+  "hola"
 };
 int (*builtin_func[]) (char **) = {
   &shell_cd,
   &shell_help,
-  &shell_exit
+  &shell_exit,
+  &shell_greet,
+  &shell_greet,
+  &shell_greet
 };
 int num_builtins() {
   return sizeof(builtin_str) / sizeof(char *);
@@ -57,6 +88,11 @@ int shell_help(char **args) {
 int shell_exit(char **args) {
     return 0;
 }
+int shell_greet(char **args) {
+    printf(MAGENTA "Hi there. Why are you talking to me. \n" RESET);
+    hiState = 1;
+    return 1;
+}
 
 
 
@@ -74,18 +110,80 @@ char *read_line(void)
     }
     while (1) {
         c = getchar();
-        if (c == EOF || c == '\n') { //if end of line set it to \0 and return the line
+        if (c == '\033') { // Escape sequence
+            getchar(); // skip the bracket '['
+            switch (getchar()) {
+                case 'A': // up arrow previous command
+                    if (history_cur_index == 0) break;
+                    printf("\r\033[K");
+                    char cwd[1024];
+                    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+                        printf(BLUE"TinyShell" RESET":" GREEN"%s" RESET "> ", cwd); // green directory path
+                    } else {
+                        printf("$ ");
+                    }
+                    if (history_cur_index > 0) {
+                        history_cur_index -= 1;
+                        strcpy(buffer, history[history_cur_index]);
+                        position = (int)strlen(buffer);
+                        printf("%s", history[history_cur_index]);
+                        
+
+                    } else {
+                        position = 0;
+                    }
+                    break;
+                case 'B': // down arrow (dont know yet)
+                    if (history_cur_index >= history_index) break;
+                    printf("\r\033[K");
+                    char cwd2[1024];
+                    if (getcwd(cwd2, sizeof(cwd2)) != NULL) {
+                        printf(BLUE"TinyShell" RESET":" GREEN"%s" RESET "> ", cwd2); // green directory path
+                    } else {
+                        printf("$ ");
+                    }
+                    
+                    history_cur_index += 1;
+                    if (history_cur_index != history_index) {
+                        strcpy(buffer, history[history_cur_index]);
+                        position = (int)strlen(buffer);
+                        printf("%s", history[history_cur_index]);
+                    } else {
+                        position = 0;
+                    }
+                        
+
+                    
+                    break;
+            }
+        } else if (c == '\t') { //tab to autocomplete
+            printf("tab");
+        } else if (c == 127 || c == 8) {
+            if (position > 0) {
+                position--;              // move cursor back
+                buffer[position] = '\0'; // erase last char in buffer
+                // erase visually
+                printf("\b \b");
+                fflush(stdout);
+            }
+        } else if (c == EOF || c == '\n') { //if end of line set it to \0 and return the line
+            printf("\n");
             buffer[position] = '\0';
+            
             return buffer; 
         } else {
-            buffer[position] = c; //else next char add it to the line
+            printf("%c", c);
+           
+            buffer[position] = c; 
+            position += 1;
+                             //else next char add it to the line
         }
-
-        position += 1;
 
         
 
-        if (position >= bufSize) { //reallocate some more space if exceeds buffer size
+        
+        //reallocate some more space if exceeds buffer size
+        if (position >= bufSize) {
             bufSize += 1024;
             buffer = realloc(buffer, bufSize * sizeof(char));
             if (!buffer) {
@@ -153,14 +251,22 @@ int shell_launch(char **args) {
 
 int shell_execute(char **args) {
     int i;
-
+    
     if (args[0] == NULL) { //empty command
+        if (hiState) {
+            printf(MAGENTA "Fine don't respond. \n" RESET);
+            hiState = 0;
+        }
         printf(RED"Wheres the command??\n"RESET);
+        
         return 1;
     }
-
+    if (hiState) {
+        printf(MAGENTA "No hablo ingles. \n" RESET);
+        hiState = 0;
+    }
     for (i = 0; i < num_builtins(); i++) {
-        if (strcmp(args[0], builtin_str[i]) == 0) {
+        if (strcasecmp(args[0], builtin_str[i]) == 0) {
             return (*builtin_func[i])(args);
         }
     }
@@ -180,6 +286,7 @@ void shell_loop(void) {
         char *line;
         char **args;
         line = read_line();
+        manageHistory(line);
         args = split_line(line);
         status = shell_execute(args);
 
@@ -191,8 +298,9 @@ void shell_loop(void) {
 
 //START THE SHELL!!!!
 int main(int argc, char **argv) {
-    chdir("/Users/akashkothari/Desktop"); //set starting directory
 
+    chdir("/Users/akashkothari/Desktop"); //set starting directory
+    enable_raw_mode(); //allow for tab and arrow key handling
     shell_loop(); //being the loop
 
     return 0;
